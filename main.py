abb106c2a527b5ddf4d2a803cd41097ff08d651b
DeltaRO1 <aciobanitei.robert@gmail.com>
2023-10-02 10:32:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,16 @@ main_win = QWidget()
 main_win.setWindowTitle('Helth')
 main_win.move(900, 70)
 main_win.resize(800, 1000)
+
+welcome = QLabel("Welcome to the Health status detection program!")
+text = QLabel("The application allows you to use the Ruffer Test to make an initial diagnosis of your health.")
+but = QPushButton("Start")
+line = QVBoxLayout()
+
+line.addWidget(welcome, alignment = Qt.AlignCenter)
+line.addWidget(text, alignment= Qt.AlignCenter)
+line.addWidget(but, alignment= Qt.AlignCenter)
+
+main_win.setLayout(line)
 main_win.show()
 app.exec_()
